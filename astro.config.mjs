a19bfbaf79e708dfd19582bc1301c88617f04b52
Luis R Zunniga S <luiscr1104@gmail.com>
2025-10-09 07:40:34 -0600

    Fase dos sin reseend

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,12 @@ import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 
-// https://astro.build/config
 export default defineConfig({
+  alias: {
+    '@': './src',
+  },
+
   vite: {
-    plugins: [tailwindcss()]
-  }
+    plugins: [tailwindcss()],
+  },
 });
